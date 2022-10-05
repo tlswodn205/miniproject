3d5d5440710004f29569b7c2e955af6bcd90a8fb
tlswodn205 <80434390+tlswodn205@users.noreply.github.com>
2022-10-05 16:19:18 +0900

    SJW - 로그인 구현 완료
    로그인기능 구현완료

--- src/main/java/site/metacoding/miniproject/service/UserService.java
+++ src/main/java/site/metacoding/miniproject/service/UserService.java
@@ -5,11 +5,22 @@ import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import site.metacoding.miniproject.domain.user.User;
 import site.metacoding.miniproject.domain.user.UserDao;
+import site.metacoding.miniproject.web.dto.LoginDto;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 	private final UserDao userDao;
 
-	
+	public User 로그인(LoginDto loginDto) {
+		User userPS = userDao.findByUsername(loginDto.getUsername());
+		if (userPS == null) {
+			return null;
+		}
+
+		if (userPS.getPassword().equals(loginDto.getPassword())) {
+			return userPS;
+		}
+		return null;
+	}
 }
--- src/main/java/site/metacoding/miniproject/web/UserController.java
+++ src/main/java/site/metacoding/miniproject/web/UserController.java
@@ -1,14 +1,35 @@
 package site.metacoding.miniproject.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
+import site.metacoding.miniproject.domain.user.User;
 import site.metacoding.miniproject.service.UserService;
+import site.metacoding.miniproject.web.dto.CMRespDto;
+import site.metacoding.miniproject.web.dto.LoginDto;
 
 @RequiredArgsConstructor
 @Controller
 public class UserController {
-	private final UserService userService;
+    private final UserService userService;
+    private final HttpSession session;
 
+    @PostMapping("/login")
+    public @ResponseBody CMRespDto<?> login(@RequestBody LoginDto loginDto) {
+
+        User principal = userService.로그인(loginDto);
+        if (principal == null) {
+            return new CMRespDto<>(-1, "로그인실패", null);
+        }
+        session.setAttribute("principal", principal);
+
+        User isLogin = (User) session.getAttribute("principal");
+        return new CMRespDto<>(1, "로그인성공", null);
+    }
 }
