9c0ee8d680e83c705f2d6ec468fa85bc2f33327b
Choi JaeYoung <chlwowudd0327@naver.com>
2022-10-11 15:23:19 +0900

    skillList add

---
+++ src/main/java/site/metacoding/miniproject/util/BasicSkillList.java
@@ -0,0 +1,25 @@
+package site.metacoding.miniproject.util;
+
+import java.util.List;
+
+public class BasicSkillList {
+	static private List<String> skillList;
+	
+	static public List<String> getSkillList() {
+		
+		skillList.add("Python");
+		skillList.add("Spring Framework ");
+		skillList.add("AWS");
+		skillList.add("Git");
+		skillList.add("iOS");
+		skillList.add("HTML");
+		skillList.add("JavaScript");
+		skillList.add("MySQL");
+		skillList.add("Linux");
+		skillList.add("Android");
+		skillList.add("Kotlin");
+		skillList.add("Swift");
+		
+		return skillList;
+	}
+}
