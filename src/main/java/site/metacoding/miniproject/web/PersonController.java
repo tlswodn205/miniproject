2756d983d75e7626bd05331d494d446409074887
Choi JaeYoung <chlwowudd0327@naver.com>
2022-10-12 11:58:43 +0900

    import List add

--- src/main/java/site/metacoding/miniproject/web/PersonController.java
+++ src/main/java/site/metacoding/miniproject/web/PersonController.java
@@ -2,6 +2,8 @@ package site.metacoding.miniproject.web;
 
 
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
